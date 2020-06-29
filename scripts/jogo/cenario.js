ed7391abac9409b2349a34e4f032096bc4ae26e9
Ami Onodera <62409630+ami-onodera@users.noreply.github.com>
2020-06-30 00:59:48 +0200

    3.4 - Solved parallax merging!

--- scripts/jogo/cenario.js
+++ scripts/jogo/cenario.js
@@ -15,10 +15,10 @@ class Cenario {
     this.x1 = this.x1 - this.velocidade;
     this.x2 = this.x2 - this.velocidade;
 
-    if (this.x1 < -width) {
+    if (this.x1 < -width+this.velocidade) {
       this.x1 = width
     }
-    if (this.x2 < -width) {
+    if (this.x2 < -width+this.velocidade) {
       this.x2 = width
     }
   }
